cbb673fb7f75068a4982c8a7c863e3b926141c28
ShilaShabiha <114427488+ShilaShabiha@users.noreply.github.com>
2022-10-28 20:21:19 +0600

    Add files via upload

---
+++ Lab 4/Home.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+</head>
+<body>
+    
+<fieldset>
+		   <div> 
+		   
+		      <p ><h1 style="color: green">  Online  <sub style="color:black">Shopping </sub> <h1> </p> 
+		 
+		 
+		   
+		       <h3 align= "center">
+		 
+		 
+		       <a style="color:SlateBlu;" href="Home.php">  Home |  </a> 
+		       <a style="color:SlateBlu;" href="Login.php">  Login |  </a>  
+		       <a style="color:SlateBlu;" href="Registration.php">  Registration </a> 
+		 
+		  
+		   </h3>
+		   
+		  <hr>
+		  
+		</div> 
+
+	    <div>
+	    	<legend>
+
+		   <p> <h1 align="left" > Welcome to <sub style="color:green" > Online <sup style="color:black">Shopping </sub> </p> <br> <br>
+		   
+		     <hr>
+	      </div>
+		   
+		   <div align="center">
+		   
+		   <h4 > <span style="color: rgb(140, 140, 140);"> Copyright ©  <?php echo date(2022);?> </span> </h4>
+		   </legend>
+		   </div>
+		   </fieldset>
+	 </body>
+</html>
